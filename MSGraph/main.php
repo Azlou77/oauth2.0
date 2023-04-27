66c360e47be3b16518e51b1dc9d7eb91548ebe70
Azlou77 <nguyenlouis172@gmail.com>
2023-04-27 12:05:39 +0200

    add templates read/send events && read mail

--- MSGraph/main.php
+++ MSGraph/main.php
@@ -1,15 +1,5 @@
-<!-- Fusion html form post and php main
-     send data post in same page -->
-
-<!-- Create form to send events -->
-<html>
-    <head>
-        <title>Send event</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    </head>
-    <body>
-        <h1>Send events</h1>
+<?php include 'header.php'; ?>
+       <h1>Send events</h1>
        <!-- Form post -->
         <form  method="POST" action="">
             <!-- Field texte -->
