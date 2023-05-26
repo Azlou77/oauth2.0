9d8766140f20cdd8d0657f0345b1754efd07aed3
Azlou77 <nguyenlouis172@gmail.com>
2023-05-26 14:21:27 +0200

    update ReadEventson calendar

--- MSGraph/PHP version/View/Form/Events/ReadEvents.php
+++ MSGraph/PHP version/View/Form/Events/ReadEvents.php
@@ -1,54 +1,70 @@
+<?php
+// Use dependencies commposer
+require_once 'vendor/autoload.php';
+require_once 'GraphHelper.php';
+
+// Use features MSGraph²
+use Microsoft\Graph\Graph;
+use Microsoft\Graph\Model;
+
+// Load .env file
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+$dotenv->required(['CLIENT_ID', 'TENANT_ID', 'GRAPH_USER_SCOPES']);
+
+// Initialisze MS-Graph client authentification
+GraphHelper::initializeGraphForAppOnlyAuth();
+$graph = new Graph();
+
+//Get the access token from MSGraph class
+$token = GraphHelper::getAppOnlyToken();
+    
+//Set the access token to the GraphHelper class
+$graph->setAccessToken($token);
+
+//Get the events
+$events = $graph->createCollectionRequest('GET', '/users/louis.nguyen@network-systems.fr/calendar/events')
+    ->setReturnType(Model\Event::class);
+$newEvents = $events->getPage();
+
+?>
 <html>
+    <head>
+        <title>Display events</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <style>
+            table, th, td {
+                border: 1px solid black;
+            }
+        </style>
+    </head>
     <body>
-      <!--Showing Calendar Events  -->
-      <h1>My Events </h1>
-
-<!-- Display events with bootstrap  -->
-<table class="table table-striped">
-    <thead>
-        <tr>
-            <th>Subject</th>
-            <th>Start</th>
-            <th>End</th>
-            <th>Attendees</th>
-            <th>Reminder</th>
-            <th>Body</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php
-        // Loop to display all events
-        foreach ($events as $event) {
-            echo '<tr>';
-            echo '<td>' . $event->getSubject() . '</td>';
-            echo '<td>' . $event->getStart()->getDateTime() . '</td>';
-            echo '<td>' . $event->getEnd()->getDateTime() . '</td>';
-            echo '<td>' . $event->getAttendees()[0]->getEmailAddress()->getAddress() . '</td>';
-            echo '<td>' . $event->getReminderMinutesBeforeStart() . '</td>';
-            echo '<td>' . $event->getBody()->getContent() . '</td>';
-            echo '</tr>';
-        }
-        ?>
-    </tbody>
-</table>    
-
-
-      <!-- Display events on card using bootstrap template -->
-      <div class="container">
-        <div class="row">
-            <?php foreach ($events as $event): ?>
-                <div class="col-sm-6 col-md-4">
-                    <div class="thumbnail">
-                        <div class="caption">
-                            <h3><?php echo $event->getSubject(); ?></h3>
-                            <p><?php echo $event->getBody()->getContent(); ?></p>
-                            <p><?php echo $event->getStart()->getDateTime(); ?></p>
-                            <p><?php echo $event->getEnd()->getDateTime(); ?></p>
-                        </div>
-                    </div>
-                </div>
-            <?php endforeach; ?>
+        <div class="container">
+            <h1>Display events</h1>
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Subject</th>
+                        <th>Body</th>
+                        <th>Start</th>
+                        <th>End</th>
+                        <th>Reminder</th>
+                        <th>Reminder minutes before start</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($newEvents as $event): ?>
+                        <tr>
+                            <td><?php echo $event->getSubject(); ?></td>
+                            <td><?php echo $event->getBody()->getContent(); ?></td>
+                            <td><?php echo $event->getStart()->getDateTime(); ?></td>
+                            <td><?php echo $event->getEnd()->getDateTime(); ?></td>
+                            <td><?php echo $event->getIsReminderOn(); ?></td>
+                            <td><?php echo $event->getReminderMinutesBeforeStart(); ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
         </div>
-      </div>
     </body>
 </html>
