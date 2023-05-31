b862237cde9b9b7c917129e4a170084ae7f604c9
Azlou77 <nguyenlouis172@gmail.com>
2023-05-31 10:21:36 +0200

    add modal onclick dayGridMonth

---
+++ MSGraph/View/include/modal.php
@@ -0,0 +1,58 @@
+ <!-- The Modal -->
+ <div class="modal" id="myModal">
+   <div class="modal-dialog modal-dialog-centered">
+
+    <!-- Form post -->a
+    <form  method="POST" action="">
+        <div class="modal-content">
+        
+            <!-- Modal Header -->
+            <div class="modal-header">
+                <h5 class="modal-title">Add an event</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+            </div>
+        
+            <!-- Modal body -->
+            <div class="modal-body">
+        
+            <!-- Input subject-->
+                <div class="mb-3">
+                    <label for="" class="form-label">subject</label>
+                    <input type="text" class="form-control" id="subject" name="subject" placeholder="Enter subject">
+            </div>
+        
+                <!-- Input  body-->
+                <div class="mb-3">
+                    <label for="body" class="form-label">body</label>
+                    <input type="text" class="form-control" id="body" name="body" placeholder="Enter body">
+                </div>
+            
+        
+                <!-- Email attendees-->
+                <div class="mb-3">
+                    <label for="attendees" class="form-label">attendees</label>
+                    <input type="email" class="form-control" id="attendees" name="attendees" placeholder="Enter attendees">
+                </div>
+        
+                <!-- Input start date-->
+                <div class="mb-3">
+                    <label for="start" class="form-label">Start date</label>
+                    <input type="date" class="form-control" id="start" name="start">
+                </div>
+
+                <!-- Input end date-->
+                <div class="mb-3">
+                    <label for="end" class="form-label">End date</label>
+                    <input type="date" class="form-control" id="end" name="end">
+                </div>
+            </div>
+            
+            <!-- Modal footer -->
+            <div class="modal-footer">
+                <button type="submit" class="btn btn-primary">Add</button>
+                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </form>
+    </div>
+</div>   
--- MSGraph/index.php
+++ MSGraph/index.php
@@ -2,10 +2,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blazor Calendar</title>
+    <title>Calendar</title>
     <!--Bootstrap assets-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
     <!--Font awesome assets-->
     <script src="https://kit.fontawesome.com/2b554022ef.js" crossorigin="anonymous"></script>
 
@@ -13,146 +16,109 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
         integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
-        <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/index.global.min.js'></script>
-        <script>
+    <!-- Jquery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script> 
 
-document.addEventListener('DOMContentLoaded', function() {
-  var calendarEl = document.getElementById('calendar');
+    <!-- Script fullcalendar Api -->
+    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/index.global.min.js'></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+        var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+          initialView: 'dayGridMonth',
+            initialDate: '2021-07-07',
+            headerToolbar: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+            },
+        // Modal onclick dayGridMonth
+        dateClick: function(info) {
+        $('#myModal').modal('show');
+        $('#start').val(info.dateStr);
+        $('#end').val(info.dateStr);
+        },
+    });
+        calendar.render();
+      });
 
-  var calendar = new FullCalendar.Calendar(calendarEl, {
-    initialView: 'dayGridMonth',
-    initialDate: '2023-05-07',
-    headerToolbar: {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'dayGridMonth,timeGridWeek,timeGridDay'
-    },
-    events: [
-        
-    ],
-  });
-  calendar.render();
-
-}
-</script>
+    </script>
 <body>
     <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">
-            <div class="ms-BrandIcon--icon48 ms-BrandIcon--outlook"></div>
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText"
-            aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarText">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
-                </li>
-            </ul>
-            <span class="navbar-text">
-                Login Section
-            </span>
-        </div>
-    </nav>
+    <?php include './View/include/navbar.php'; ?>
 
     <!-- Header -->
     <header>
         <h3 class="ms-fontSize-42 ms-fontWeight-regular">Welcome to Blazor Calendar</h3>
         <p class="ms-fontSize-14 ms-fontWeight-regular">Blaozr Calendar allows you to manage all you events within your
             Outlook Calendar</p>
-
         <!-- NotAuthenticated Users-->
         <p class="ms-fontSize-14 ms-fontWeight-regular">Please Sign in with your Micorosft Account to get started</p>
         <a href="#" class="microsoft-login-button shadow-effect"><img src="assets/images/microsoft.png" alt=""> Sign in
             with Microsoft</a>
-        
     </header>
 
     <div class="row">
-    <div id='calendar'></div>
+        <div id='calendar'></div>
+         <!-- The Modal -->
+ <div class="modal" id="myModal">
+   <div class="modal-dialog modal-dialog-centered">
 
-       
-                      
-                            <div class="day shadow-effect">
-                                <h3 class="ms-fontSize-24 ms-fontWeight-regular">17</h3>
+    <!-- Form post -->
+    <form  method="POST" action="">
+        <div class="modal-content">
+        
+            <!-- Modal Header -->
+            <div class="modal-header">
+                <h5 class="modal-title">Add an event</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+            </div>
+        
+            <!-- Modal body -->
+            <div class="modal-body">
+        
+            <!-- Input subject-->
+                <div class="mb-3">
+                    <label for="" class="form-label">subject</label>
+                    <input type="text" class="form-control" id="subject" name="subject" placeholder="Enter subject">
+            </div>
+        
+                <!-- Input  body-->
+                <div class="mb-3">
+                    <label for="body" class="form-label">body</label>
+                    <input type="text" class="form-control" id="body" name="body" placeholder="Enter body">
+                </div>
+            
+        
+                <!-- Email attendees-->
+                <div class="mb-3">
+                    <label for="attendees" class="form-label">attendees</label>
+                    <input type="email" class="form-control" id="attendees" name="attendees" placeholder="Enter attendees">
+                </div>
+        
+                <!-- Input start date-->
+                <div class="mb-3">
+                    <label for="start" class="form-label">Start date</label>
+                    <input type="date" class="form-control" id="start" name="start">
+                </div>
 
-                                  <!-- Button modal-->
-                                  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">
-                                    Open modal
-                                  </button>
-                                </div>
-                                
-                                <!-- The Modal -->
-                                <div class="modal" id="myModal">
-                                  <div class="modal-dialog modal-dialog-centered">
-                                  <!-- Form post -->
-                                  <form  method="POST" action="">
-                                  <div class="modal-content">
-                                
-                                      <!-- Modal Header -->
-                                      <div class="modal-header">
-                                        <h5 class="modal-title">Add an event</h5>
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-                                      </div>
-                                
-                                      <!-- Modal body -->
-                                      <div class="modal-body">
-                                
-                                      <!-- Input subject-->
-                                        <div class="mb-3">
-                                            <label for="" class="form-label">subject</label>
-                                            <input type="text" class="form-control" id="subject" name="subject" placeholder="Enter subject">
-                                      </div>
-                                
-                                        <!-- Input  body-->
-                                        <div class="mb-3">
-                                            <label for="body" class="form-label">body</label>
-                                            <input type="text" class="form-control" id="body" name="body" placeholder="Enter body">
-                                        </div>
-                                   
-                                
-                                        <!-- Email attendees-->
-                                        <div class="mb-3">
-                                            <label for="attendees" class="form-label">attendees</label>
-                                            <input type="email" class="form-control" id="attendees" name="attendees" placeholder="Enter attendees">
-                                        </div>
-                                
-                                        <!-- Input start date-->
-                                        <div class="mb-3">
-                                            <label for="start" class="form-label">Start date</label>
-                                            <input type="date" class="form-control" id="start" name="start">
-                                        </div>
-
-                                
-                                        <!-- Input end date-->
-                                        <div class="mb-3">
-                                            <label for="end" class="form-label">End date</label>
-                                            <input type="date" class="form-control" id="end" name="end">
-                                        </div>
-
-                                      </div>
-                                    
-
-                                
-                                    
-                                      <!-- Modal footer -->
-                                      <div class="modal-footer">
-                                        <button type="submit" class="btn btn-primary">Add</button>
-                                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
-                                      </div>
-                                
-                                </div>
-                                </form>
-                               </div>
-                             </div>   
-                        
-                 
-             
+                <!-- Input end date-->
+                <div class="mb-3">
+                    <label for="end" class="form-label">End date</label>
+                    <input type="date" class="form-control" id="end" name="end">
+                </div>
+            </div>
+            
+            <!-- Modal footer -->
+            <div class="modal-footer">
+                <button type="submit" class="btn btn-primary">Add</button>
+                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </form>
+    </div>
+</div>   
+  
 </body>
 </html>
 
