d7522f933666d132a8cf1d34863b425dea25fd32
Azlou77 <nguyenlouis172@gmail.com>
2023-06-01 10:41:51 +0200

    add pdo connection

--- MSGraph/test/save.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $url = "https://graph.microsoft.com/v1.0/me/messages/{id}/$value";
-    $response = $client->get($url);
-    $body = $response->getBody();
-    $decodedBody = base64_decode($body);
-    file_put_contents("message.eml", $decodedBody);
-?>
