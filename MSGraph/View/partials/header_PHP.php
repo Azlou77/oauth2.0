a012adcd98116b0de3adba79acd21be8e6fe4a17
Nguyen Louis <nguyenlouis172@gmail.com>
2023-07-06 12:47:23 +0200

    update header partials

---
+++ MSGraph/View/partials/header_PHP.php
@@ -0,0 +1,23 @@
+<?php
+// Use dependencies commposer
+require_once 'vendor/autoload.php';
+require_once 'GraphHelper.php';
+
+// Use features MSGraph²
+use Microsoft\Graph\Graph;
+use Microsoft\Graph\Model;
+
+// Load .env file
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+$dotenv->required(['CLIENT_ID', 'TENANT_ID', 'GRAPH_USER_SCOPES']);
+
+// Initialisze MS-Graph client authentification
+GraphHelper::initializeGraphForAppOnlyAuth();
+$graph = new Graph();
+
+//Get the access token from MSGraph class
+$token = GraphHelper::getAppOnlyToken();
+    
+//Set the access token to the GraphHelper class
+$graph->setAccessToken($token);
